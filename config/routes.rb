bf71fef23dddabd03915455e567bd94ba7ca1660
Rickie Natividad <rickienatividad85@gmail.com>
2022-09-25 10:21:39 -0400

    adds sessions controller and post action. Adds a user to seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   #user
   post "/users" => "users#create"
+
+  #sessions
+  post "/sessions" => "sessions#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.new(
+  name: "mojo jojo",
+  email: "mjjj@test.com",
+  password: "password",
+  password_confirmation: "password",
+)
+user.save
